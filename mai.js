2773f102302c33a10a7c32a926fdc358ab5533ae
multiartdg <multiart.dg@gmail.com>
2024-09-04 02:24:27 +0200

    Add files via upload

---
+++ mai.js
@@ -0,0 +1,12 @@
+
+const nu1 = prompt("Ingresa el numero 1")
+const nu2 = prompt("Ingresa el primer 2")
+const nu3 = prompt("Ingresa el primer 3")
+
+const nulInt = Number(nu1)
+const nu2Int = Number(nu2)
+const nu3Int = Number(nu3)
+
+const resultado = nulInt + nu2Int + nu3Int
+const promedio = resultado / 3
+alert(" Promedio de los tres nuemro es: " + promedio )
